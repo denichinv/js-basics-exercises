54a8d1a25b8b5ed882964c3299ade0610f535580
denichinv <denichinv@gmail.com>
2024-05-24 16:20:21 +0100

    Add files via upload

---
+++ convertMtoKm.js
@@ -0,0 +1,7 @@
+function ConvMtoKm(meters) {
+
+    let distanceInKm = meters / 1000;
+
+    console.log(distanceInKm.toFixed(2));
+}
+ConvMtoKm (1852)
---
+++ poundsToDollars.js
@@ -0,0 +1,9 @@
+function poundsToDollars(dollars) {
+
+let valueInPounds = dollars * 1.31; 
+
+console.log(valueInPounds.toFixed(3));
+    
+}
+
+poundsToDollars (80)
